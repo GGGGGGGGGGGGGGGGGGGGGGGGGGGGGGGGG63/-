4c86578473fa00acb9b4eb728c7777b768c20ba3
GGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGG63 <51705641+GGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGG63@users.noreply.github.com>
2019-06-19 18:58:16 +0800

    Initial Commit

---
+++ 程序/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  程序
+//
+//  Created by s20181105880 on 2019/6/19.
+//  Copyright © 2019 s20181105880. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
