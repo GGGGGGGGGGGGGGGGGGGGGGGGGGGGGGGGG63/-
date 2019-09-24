16a338bbeacd29e48964df7adffab5df2b01eb88
GGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGG63 <51705641+GGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGG63@users.noreply.github.com>
2019-09-24 16:49:53 +0800

    指针2
    指针2

--- 程序/main.c
+++ 程序/main.c
@@ -10,14 +10,13 @@
     
     int main(int argc, const char * argv[]) {
         // insert code here...
-        int *p;
-        int i;
-        p=&i;
-        *p=100;
+        static signed int i=0xAABBCCDD;
+        char *p;
+        int j=10;
+        p=(char *)&i;
         printf("p=%x\n",p);
-        printf("&p=%x\n",&p);
-        printf("*p=%x\n",*p);
-        printf("i=%x\n",i);
         printf("&i=%x\n",&i);
+        printf("*(p+3)=%x\n",*(p+3));
+        printf("*p=%x\n",*p);
         return 0;
     }
